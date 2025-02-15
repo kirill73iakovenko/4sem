2e86b211f9cbf2dd8cd3e371ca62496428390f2c
kirill73iakovenko <144524981+kirill73iakovenko@users.noreply.github.com>
2025-02-15 16:54:53 +0300

    1

---
+++ 1/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+template <typename Container>
+void Print(const Container container, const string str) {
+    bool indicator = true;
+    for (auto element : container) {
+        if (!indicator) {
+            cout << str;
+        }
+        cout << element;
+        indicator = false;
+    }
+    cout << '\n';
+};
+
+int main() {
+    vector<int> data = { 1, 2, 3 };
+    vector<string> colors = { "yellow", "green", "blue" };
+    Print(data, ", ");
+    Print(colors, "; ");
+};
