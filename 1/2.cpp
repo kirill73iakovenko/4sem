a8c2352a0a230622f66e2c9090d280b1f8c5989f
kirill73iakovenko <144524981+kirill73iakovenko@users.noreply.github.com>
2025-02-16 18:00:02 +0300

    5

--- 1/2.cpp
+++ 1/2.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 template <typename T>
-void Sort(list<T>& List_o, list<T>& List_p, list<T>& List_n) {
+void Sort(list<T>* List_o, list<T>* List_p, list<T>* List_n) {
 
-    for (auto num : List_o) {
+    for (auto num : *List_o) {
         if (num > 0) {
-            List_p.push_back(num);
+            List_p->push_back(num);
         }
         else if (num < 0) {
-            List_n.push_back(num);
+            List_n->push_back(num);
         }
     }
 
-    List_p.sort();
-    List_n.sort();
+    List_p->sort();
+    List_n->sort();
 
 };
 
@@ -38,7 +38,7 @@ int main() {
     list<int> list1 = { 3, -2, 9, 8, 5, 12, -18, -100, 56, 83, -12 };
     list<int> list_plus;
     list<int> list_minus;
-    Sort(list1, list_plus, list_minus);
+    Sort(&list1, &list_plus, &list_minus);
     cout << "positive num: ";
     Print(list_plus, ", ");
     cout << "\n";
